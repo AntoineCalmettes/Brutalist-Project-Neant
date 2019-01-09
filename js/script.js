83c5afaa288ada213dec4cd6f355f838d9eb5a9d
antoine <calmettes.antoine2@gmail.com>
2019-01-09 19:43:47 +0100

    player audio

--- js/script.js
+++ js/script.js
@@ -60,17 +60,67 @@ function flicker() {
 }());
 
 // ----------------------------------------------------------------------------------
-// let stockLink = document.getElementsByClassName("lol");
-//
-// function mouveBackground(link){
-//   link[0].style.background = "linear-gradient(to bottom, #00FDFF 0%, #FE00FF 100%)";
-//   link[1].style.background = "linear-gradient(to bottom, #00FDFF 0%, #FE00FF 100%)";
-//   setTimeout(mouveBackground(link),1100)
-// }
-// function mouveBackground2(link){
-//   link[0].style.background = "linear-gradient(to bottom, #FE00FF 0%, #00FDFF 100%)";
-//   link[1].style.background = "linear-gradient(to bottom, #FE00FF 0%, #00FDFF 100%)";
-//   setTimeout(mouveBackground2(link),500)
-// }
-// mouveBackground(stockLink);
-// mouveBackground2(stockLink);
+// Most of this is all Dave Rupert
+// See https://codepen.io/davatron5000/pen/uqktG
+
+(function(){
+  $(".play-pause").click(function() {
+
+   if($(this).hasClass('pausing')){
+      $(this).removeClass('pausing');
+      $(this).addClass('playing');
+      $(this).css("background-image", "url(https://media.giphy.com/media/3oEhmM10mIi1dkMfmg/giphy.gif)");
+      audio.play();
+  }
+  else if($(this).hasClass('playing')){
+      $(this).removeClass('playing');
+      $(this).addClass('pausing');
+      $(this).css("background-image", "url(https://media.giphy.com/media/3oEhmM10mIi1dkMfmg/giphy.gif)");
+      audio.pause();
+  }
+  else {
+    $(this).addClass('playing');
+    $(this).css("background-image", "url(https://media.giphy.com/media/3oEhmM10mIi1dkMfmg/giphy.gif)");
+    audio.play();
+  }
+
+});
+
+
+
+  var pcastPlayers = document.querySelectorAll('.player-container');
+  var speeds = [ 1, 1.5, 2, 2.5, 3, 0.5 ]
+
+  for(i=0;i<pcastPlayers.length;i++) {
+    var player = pcastPlayers[i];
+    var audio = player.querySelector('audio');/*
+    var play = player.querySelector('.play-pause.playing');
+    var pause = player.querySelector('.play-pause.pausing');*/
+
+
+
+    var progress = player.querySelector('.pcast-progress');
+
+    var currentTime = player.querySelector('.pcast-currenttime');
+
+    var currentSpeedIdx = 0;
+
+
+    var toHHMMSS = function ( totalsecs ) {
+        var sec_num = parseInt(totalsecs, 10); // don't forget the second param
+        var hours   = Math.floor(sec_num / 3600);
+        var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+        var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+        if (hours   < 10) {hours   = "0"+hours; }
+        if (minutes < 10) {minutes = ""+minutes;}
+        if (seconds < 10) {seconds = "0"+seconds;}
+
+        var time = hours+':'+minutes+':'+seconds;
+        if (hours   <= 1) { var time = minutes+':'+seconds; }
+
+        return time;
+    }
+
+  }
+})(this);
